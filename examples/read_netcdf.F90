4aa55d99f27b4b874598b83e081d59cf0bf2169f
Matthew Bishop <bishop@atmos.colostate.edu>
2012-03-01 16:33:00 -0700

    example read and write code

---
+++ examples/read_netcdf.F90
@@ -0,0 +1,33 @@
+#include <simple_netcdf.nc>
+
+subroutine read_netcdf
+    type(SNCFile) :: file
+    type(SNCVar) :: t_var
+    integer :: lat_size, lon_size, time_size
+    real*4, pointer :: t(:,:)
+    character(32) :: calendar, units, long_name
+
+    ! Open the file for reading.
+    file = snc_open("some.nc")
+
+    ! Read a global attribute
+    call snc_get_global_att(file, "calendar", calendar)
+
+    ! If you need to, get the dimensions of the file.
+    lat_size = snc_get_dim(file, "lat")
+    lon_size = snc_get_dim(file, "lon")
+    time_size = snc_get_dim(file, "time")
+
+    ! Find the variable in the file by name.  Optionally retrieve its units.
+    t_var = snc_inq_var(file, "t", units)
+    ! The number and size of t's dimensions is automatically read:
+    print *, "t has", t_var%ndims, "dimensions"
+    print *, "t's dimension sizes are:", t_var%dims(1:t_var%ndims)
+
+    ! Read the variable data into t.
+    call snc_read(file, t_var, t)
+
+    ! Close the file once we're done with it.
+    call snc_close(file)
+
+end subroutine read_netcdf
---
+++ examples/write_cf_netcdf.F90
@@ -0,0 +1,76 @@
+#include <simple_netcdf.inc>
+
+! Write a CF-compliant gridded NetCDF file
+subroutine cf_netcdf
+    use SimpleNetCDF
+    implicit none
+
+    ! variables to keep track of NetCDF things
+    type(SNCFile) :: file
+    type(SNCVar) :: var, zvar
+
+    ! grid and coordinate variables
+    integer, parameter :: N_LAT = 3, N_LON = 4, N_LEVELS = 2, N_TIME = 2
+    real*4 :: lat(N_LAT), lon(N_LON), time(N_TIME)
+    real*4 :: d(N_LON, N_LAT, N_LEVELS, N_TIME)
+    character(60) :: time_units
+
+    ! -- model writes data to variable d --
+
+    ! Fill in the timestamp for the start of the data in this file.
+    ! The full format is "TIME_UNIT since YYYY-MM-DD HH:MM:SS +HH:MM" where
+    ! TIME_UNIT is usually one of: seconds, hours, days.  This is followed by
+    ! the date and time and a time zone offset.  If we use UTC, we can omit
+    ! the time zone and if we start at the beginning of the day we can omit
+    ! the time.  
+    time_units = "hours since 2012-02-03 00:00:00 -07:00" ! MST
+    time_units = "hours since 2012-02-03"                 ! shortened UTC
+
+    ! Create the file.  Arguments: file name, grid cell count in the X
+    ! and Y dimensions, start time of the file data, the calendar to
+    ! use, optional file title and source, and source file and line number
+    ! preprocessor magic to help error reporting.
+    file = snc_cf_grid_create("cf.nc", N_LON, N_LAT, &
+        time_units, SNC_STANDARD_CAL, &
+        ! optional
+        title = "What Kind Of Data", source = "MyShinyProgram 1.0", &
+        ! optional, helps with debugging
+        src_file = __FILE__, src_line = __LINE__)
+
+    ! Define the vertical dimension.
+    ! Arguments: type(SNCFile) from snc_cf_grid_create, vertical dimension size,
+    ! short and long names for the variable, vertical units, does the variable
+    ! increase going "down" or "up"? and optional standard name and debugging
+    ! info.
+    zvar = snc_cf_grid_vertical(file, N_LEVELS, "levels", "Levels", "hPa", &
+        "down", &
+        ! optional, helps with debugging
+        src_file = __FILE__, src_line = __LINE__)
+
+    ! Define a variable for the file to store.  You can have more than one of
+    ! these.  You will need to create separate type(SNCVar) variables for them.
+    ! Arguments: type(SNCFile) var from snc_cf_grid_create, short variable name,
+    ! longer variable name, type (SNC_INT, SNC_FLOAT or SNC_DOUBLE), units,
+    ! and some optional arguments.
+    var = snc_cf_def_var(file, "co2", "CO2 flux", SNC_FLOAT, "kg m-2 s-1", &
+        ! optional
+        missing_value = -999.0, &
+        ! optional, helps with debugging
+        src_file = __FILE__, src_line = __LINE__)
+
+    ! Define additional attributes here as needed, for example:
+    call snc_put_att(file, var, "TracerNum", 1)
+
+    ! when you're done definining variables and attributes, call this
+    call snc_enddef(file)
+
+    ! now you need to write the coordinate variable data describing the grid
+    call snc_cf_write_coords(file, lon, lat, time)
+
+    ! write the variable data.  If you defined more than one variable above,
+    ! add other snc_write() statements for them too.
+    call snc_write(file, var, d)
+
+    ! and finally, close the file
+    call snc_close(file)
+end subroutine cf_netcdf
